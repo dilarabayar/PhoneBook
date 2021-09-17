f364c8ba2b603b6ee15d424c3bf1aaebeb04eee3
dilarabayar <dilara.bayar55@gmail.com>
2021-09-18 00:45:10 +0300

    Adding Controller

--- src/main/java/com/example/dilarabayar/phonebook/ContactController.java
+++ src/main/java/com/example/dilarabayar/phonebook/ContactController.java
@@ -4,9 +4,7 @@ import com.example.dilarabayar.phonebook.model.Contact;
 import com.example.dilarabayar.phonebook.service.ContactService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,11 @@ public class ContactController {
     public ResponseEntity<List<Contact>> getAllEmployees () {
         List<Contact> employees = contactService.listAllContacts();
         return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Contact> addContact(@RequestBody Contact contact){
+        Contact newContact = contactService.addContact(contact);
+        return new ResponseEntity<>(newContact, HttpStatus.CREATED);
     }
 }
